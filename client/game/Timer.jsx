8d8eb83bd5cc7f18da5ed70e741e7c7200c7fe4b
Ben Prystawski <11021815+benpry@users.noreply.github.com>
2022-05-16 13:56:05 -0700

    added green bar to the timer and updated the puzzles to be selected based on hill-climbability

--- client/game/Timer.jsx
+++ client/game/Timer.jsx
@@ -5,11 +5,12 @@ import Timer from "./Timer.jsx";
 
 class timer extends React.Component {
   render() {
-    const { remainingSeconds } = this.props;
+    const { remainingSeconds, stage } = this.props;
     const minutes = ("0" + Math.floor(remainingSeconds / 60)).slice(-2);
     const seconds = ("0" + (remainingSeconds - minutes * 60)).slice(-2);
-  
-  
+    const duration = stage.durationInSeconds
+    const propDone = Math.round((1 - remainingSeconds / duration) * 100)
+      
     const classes = ["timer"];
     if (remainingSeconds <= 5) {
       classes.push("lessThan5");
@@ -21,6 +22,12 @@ class timer extends React.Component {
       <div className={classes.join(" ")}>
         <h5 className='bp3-heading'>Timer</h5>
         <span className="seconds">{minutes}:{seconds}</span>
+        <div className="progress-bar-outline">
+          <div
+           className="progress-bar"
+           style={{width: `${propDone}%`}}
+          />
+        </div>
       </div>
     );
   }
